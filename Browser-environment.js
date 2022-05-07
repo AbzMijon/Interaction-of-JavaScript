190f233525e620b5b9d976fbdbe26c6be3e53329
AbzMijon <egorchiktop228@gmail.com>
2022-05-07 17:30:01 +0300

    add function hello

--- Browser-environment.js
+++ Browser-environment.js
@@ -3,7 +3,7 @@ const mainRoot = document.getElementById('root'); //We announce our variable
 const someText = mainRoot.querySelectorAll('.text'); //Then we didnt write DOCUMENT. because it big and long proccess in JS (p.s we can use if we have nesting!)
 //We can interaction with our variable , for example..
 
-// Task 1 = We need for of our variable 'someText' because it is some half array
+// Task 1 = We need forOf/for/forEach our variable 'someText' because it is some half array
 for (const iterator of someText) {
     iterator.style.color = 'red'; //What we need to do
 }
@@ -21,3 +21,24 @@ newElem.style.display = 'inline-block';
 newElem.style.border = '4px solid purple';
 
 //BOM - Browser Object Model ( [location] [history] [screen] ) We can check information about user
+
+//With For
+for (let i = 0; i < 11; i++) {
+    const someElem = document.createElement('div');
+    someElem.style.width = '500px';
+    someElem.style.height = '250px';
+    someElem.style.backgroundColor = 'blue';
+    someElem.style.margin = '15px';
+    mainRoot.append(someElem);
+}
+
+//Task with 'Hello ${name}';
+const someFunction = name => {
+    const userTag = document.createElement('h2');
+    const userName = document.createTextNode(`Wow, Welcome to team, ${name}!`);
+    userTag.style.display = 'inline-block';
+    userTag.style.margin = '250px';
+    userTag.append(userName);
+    mainRoot.append(userTag);
+}
+someFunction('Егор');
