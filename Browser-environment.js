3e3ae32d5deb43f31def73d85d7bec67f871d07e
AbzMijon <egorchiktop228@gmail.com>
2022-05-07 14:46:47 +0300

    creating element

--- Browser-environment.js
+++ Browser-environment.js
@@ -1,1 +1,17 @@
-//Will starting tomorrow)
+//In the begining we need to announce variable with helping document.
+const mainRoot = document.getElementById('root'); //We announce our variable
+const someText = mainRoot.querySelectorAll('.text'); //Then we didnt write DOCUMENT. because it big and long proccess in JS (p.s we can use if we have !nesting!)
+//We can enviroument with our variable , for example..
+
+// Task 1 = We need for of our variable 'someText' because it is some half array
+for (const iterator of someText) {
+    iterator.style.color = 'red'; //What we need to do
+}
+//But we can say one element in array to do something , for example..
+someText[2].style.color = 'green';
+
+//Lets create new element for HTML in JavaScript!
+const newElem = document.createElement('h1'); //Add tag
+const textForNewElem = document.createTextNode('It is new Element!'); //Add text or what we need
+newElem.append(textForNewElem);//Add TAG + TEXT
+mainRoot.append(newElem);//Add New Element in Our Block of Node
