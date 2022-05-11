8428b8750159cdd7082d8048da04e2ae28bb941f
AbzMijon <egorchiktop228@gmail.com>
2022-05-11 19:21:15 +0300

    finished work

--- Browser-environment.js
+++ Browser-environment.js
@@ -1,4 +1,4 @@
-//Hi, everybody! Today Challendge Only JavaScript Without HTML and CSS ------------------------------------------------------------------------------------
+//Hi, everybody! Today Challendge Only JavaScript with CSS ------------------------------------------------------------------------------------
 
 
 //In The Beginning We Announce All Variables..
@@ -9,7 +9,6 @@ const textAddBtn = document.createTextNode('Add');
 const theNote = document.createElement('input');
 const delBtn = document.createElement('button');
 const textDelBtn = document.createTextNode('Delete All');
-const allBtn = document.getElementsByTagName('button');
 
 //Then We Add To MainRoot Variables..
 mainRoot.append(toolsRow);
@@ -19,22 +18,7 @@ toolsRow.append(theNote);
 delBtn.append(textDelBtn);
 toolsRow.append(delBtn);
 
-//Add Styles For Variables..
-mainRoot.style.backgroundColor = '#dad8d8';
-mainRoot.style.maxWidth = '1150px';
-mainRoot.style.margin = '150px auto';
-mainRoot.style.padding = '25px 50px';
-mainRoot.style.border = '7px solid #000';
-mainRoot.style.borderRadius = '20px';
-toolsRow.style.width = '100%';
-toolsRow.style.display = 'grid';
-toolsRow.style.gridTemplateColumns = '1fr 2.5fr 1fr';
-toolsRow.style.columnGap = '30px';
-toolsRow.style.marginBottom = '100px';
-theNote.placeholder = 'Enter To Do...';
-addBtn.style.padding = '35px';
-delBtn.style.padding = '35px';
-theNote.style.padding = '35px';
+theNote.placeholder = 'Enter To Do...'; //For placeholder
 
 //Now we create cycle for creating new notes
 for (let i = 0; i < 2; i++) {
@@ -48,6 +32,7 @@ for (let i = 0; i < 2; i++) {
     const newCardCloseTxt = document.createTextNode('✖');
     const newCardDate = document.createElement('div');
     const newCardDateTxt = document.createTextNode('Date');
+    
     //Adding variables..
     newCardDate.append(newCardDateTxt);
     newCard.append(newCardDate);
@@ -58,58 +43,16 @@ for (let i = 0; i < 2; i++) {
     newCardComplete.append(newCardCompleteTxt);
     newCard.append(newCardComplete);
     mainRoot.append(newCard)
-    //Do styles for the same elements.
-    for (const iterator of allBtn) {
-        iterator.style.border = '5px solid #000';
-        iterator.style.borderRadius = '15px';
-        iterator.style.fontWeight = 'bold';
-        iterator.style.fontSize = '30px';
-        iterator.style.backgroundColor = '#0ff0ff';
-        iterator.style.cursor = 'pointer';
-    }
-    //Add styles..
-    newCardToDo.style.color = 'grey'; //newCardToDo
-    newCardToDo.style.backgroundColor = '#fff';
-    newCardToDo.style.padding = '10px 20px';
-    newCardToDo.style.margin = '60px 0';
-    newCardToDo.style.borderRadius = '10px';
-    newCardToDo.style.overflowY = 'auto';
-    newCardToDo.style.fontWeight = 'bold';
-    newCardToDo.style.width = '100%';
-    newCardToDo.style.minHeight = '100px';
-    newCardToDo.style.fontSize = '40px';
-    newCardToDo.style.textAlign = 'center';
-    newCard.style.padding = '15px 170px 15px 20px'; //newCard
-    newCard.style.maxWidth = '100%';
-    newCard.style.position = 'relative';
-    newCard.style.border = '6px solid #000';
-    newCard.style.borderRadius = '20px';
-    newCard.style.display = 'flex';
-    newCard.style.alignItems = 'center';
-    newCard.style.flexDirection = 'row-reverse';
-    newCard.style.justifyContent = 'left';
-    newCard.style.marginTop = '30px';
-    newCardComplete.style.padding = '10px 20px'; //newCardComplete
-    newCardComplete.style.marginRight = '15px';
-    newCardClose.style.padding = '10px 20px'; //newCardClose
-    newCardClose.style.position = 'absolute';
-    newCardClose.style.top = '10px';
-    newCardClose.style.right = '10px';
-    newCardDate.style.backgroundColor = '#fff'; //newCardDate
-    newCardDate.style.padding = '10px 35px';
-    newCardDate.style.textAlign = 'center';
-    newCardDate.style.fontSize = '30px';
-    newCardDate.style.color = 'grey';
-    newCardDate.style.letterSpacing = '1px';
-    newCardDate.style.fontWeight = 'bold';
-    newCardDate.style.position = 'absolute';
-    newCardDate.style.bottom = '15px';
-    newCardDate.style.right = '15px';
-    theNote.style.borderRadius = '15px'; //theNote
-    theNote.style.textAlign = 'center';
-    theNote.style.fontSize = '30px';
-    theNote.style.outline = 'none';
-    theNote.style.minWidth = '270px';
-    theNote.style.fontWeight = 'bold';
-    theNote.style.border = '5px solid #000';
+
+    //Give name our variables for future working in CSS
+    mainRoot.className = 'main__root';
+    toolsRow.className = 'root__row';
+    addBtn.className = 'root__add-btn';
+    theNote.className = 'root__note';
+    delBtn.className = 'root__del-btn';
+    newCard.className = 'root__new-card';
+    newCardComplete.className = 'root__complete-btn';
+    newCardToDo.className = 'root__ToDo';
+    newCardClose.className = 'root__close-btn';
+    newCardDate.className = 'root__date-btn';
 }
