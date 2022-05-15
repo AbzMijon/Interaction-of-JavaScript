f8533847173c749bf1dab85a1a577835d681e980
AbzMijon <egorchiktop228@gmail.com>
2022-05-15 21:36:27 +0300

    games into events

--- Browser-environment.js
+++ Browser-environment.js
@@ -21,7 +21,7 @@ toolsRow.append(delBtn);
 theNote.placeholder = 'Enter To Do...'; //For placeholder
 
 //Now we create cycle for creating new notes
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 1; i++) {
     //Announce variables..
     const newCard = document.createElement('div');
     const newCardComplete = document.createElement('button');
@@ -55,4 +55,47 @@ for (let i = 0; i < 2; i++) {
     newCardToDo.className = 'root__ToDo';
     newCardClose.className = 'root__close-btn';
     newCardDate.className = 'root__date-btn';
+
+    //Events
+    addBtn.addEventListener('click', () => {
+        const newCard = document.createElement('div');
+        const newCardComplete = document.createElement('button');
+        const newCardCompleteTxt = document.createTextNode('✔');
+        const newCardToDo = document.createElement('div');
+        const newCardToDoTxt = document.createTextNode('Todo Text');
+        const newCardClose = document.createElement('button');
+        const newCardCloseTxt = document.createTextNode('✖');
+        const newCardDate = document.createElement('div');
+        const newCardDateTxt = document.createTextNode('Date');
+        
+        //Adding variables..
+        newCardDate.append(newCardDateTxt);
+        newCard.append(newCardDate);
+        newCardClose.append(newCardCloseTxt);
+        newCard.append(newCardClose);
+        newCardToDo.append(newCardToDoTxt);
+        newCard.append(newCardToDo);
+        newCardComplete.append(newCardCompleteTxt);
+        newCard.append(newCardComplete);
+        mainRoot.append(newCard)
+
+        mainRoot.className = 'main__root';
+        toolsRow.className = 'root__row';
+        addBtn.className = 'root__add-btn';
+        theNote.className = 'root__note';
+        delBtn.className = 'root__del-btn';
+        newCard.className = 'root__new-card';
+        newCardComplete.className = 'root__complete-btn';
+        newCardToDo.className = 'root__ToDo';
+        newCardClose.className = 'root__close-btn';
+        newCardDate.className = 'root__date-btn';
+
+
+        newCardClose.addEventListener('click', () => {
+            newCard.style.display = 'none';
+        })
+    })
+    newCardClose.addEventListener('click', () => {
+        newCard.style.display = 'none';
+    })
 }
