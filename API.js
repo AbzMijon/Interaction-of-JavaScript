912d809dfc9ca3b35a070639eb86868a62737c59
AbzMijon <egorchiktop228@gmail.com>
2022-05-17 20:43:59 +0300

    in progress

---
+++ API.js
@@ -0,0 +1,8 @@
+const root = document.getElementById('root');
+const tx = document.getElementById('txarea');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    localStorage.setItem('txValue', tx.value);
+})
+tx.value = localStorage.getItem('txValue');
