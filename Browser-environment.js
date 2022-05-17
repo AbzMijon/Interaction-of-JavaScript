96e939257cc2d30c4c69eb807d0bc448e3963904
AbzMijon <egorchiktop228@gmail.com>
2022-05-17 17:53:49 +0300

    fixed big code to little

--- Browser-environment.js
+++ Browser-environment.js
@@ -1,7 +1,7 @@
 //Hi, everybody! Today Challendge Only JavaScript with CSS ------------------------------------------------------------------------------------
 
 
-//In The Beginning We Announce All Variables..
+//In The Beginning We Announce Variables..
 const mainRoot = document.getElementById('root');
 const toolsRow = document.createElement('div');
 const addBtn = document.createElement('button');
@@ -20,44 +20,17 @@ toolsRow.append(delBtn);
 
 theNote.placeholder = 'Enter To Do...'; //For placeholder
 
-//Now we create cycle for creating new notes
-
-    //Announce variables..
-    const newCard = document.createElement('div');
-    const newCardComplete = document.createElement('button');
-    const newCardCompleteTxt = document.createTextNode('✔');
-    const newCardToDo = document.createElement('div');
-    const newCardToDoTxt = document.createTextNode('Todo Text');
-    const newCardClose = document.createElement('button');
-    const newCardCloseTxt = document.createTextNode('✖');
-    const newCardDate = document.createElement('div');
-    const newCardDateTxt = document.createTextNode('Date');
-    
-    //Adding variables..
-    newCardDate.append(newCardDateTxt);
-    newCard.append(newCardDate);
-    newCardClose.append(newCardCloseTxt);
-    newCard.append(newCardClose);
-    newCardToDo.append(newCardToDoTxt);
-    newCard.append(newCardToDo);
-    newCardComplete.append(newCardCompleteTxt);
-    newCard.append(newCardComplete);
-
     //Give name our variables for future working in CSS
     mainRoot.className = 'main__root';
     toolsRow.className = 'root__row';
     addBtn.className = 'root__add-btn';
     theNote.className = 'root__note';
     delBtn.className = 'root__del-btn';
-    newCard.className = 'root__new-card';
-    newCardComplete.className = 'root__complete-btn';
-    newCardToDo.className = 'root__ToDo';
-    newCardClose.className = 'root__close-btn';
-    newCardDate.className = 'root__date-btn';
     
 
-    //Events
+    //Add new card
     addBtn.addEventListener('click', () => {
+        //Announce variables..
         const newCard = document.createElement('div');
         const newCardComplete = document.createElement('button');
         const newCardCompleteTxt = document.createTextNode('✔');
@@ -80,6 +53,7 @@ theNote.placeholder = 'Enter To Do...'; //For placeholder
         newCard.append(newCardComplete);
         mainRoot.append(newCard)
 
+        //Give name our variables for future working in CSS
         mainRoot.className = 'main__root';
         toolsRow.className = 'root__row';
         addBtn.className = 'root__add-btn';
@@ -91,21 +65,23 @@ theNote.placeholder = 'Enter To Do...'; //For placeholder
         newCardClose.className = 'root__close-btn';
         newCardDate.className = 'root__date-btn';
 
+        //Events
+
         //Delete All Function
         const arrCard = document.getElementsByClassName('root__new-card');
-
         delBtn.addEventListener('click', () => {
             for (const card of arrCard) {
                 card.remove();
             }
         })
 
-        //Delete One Element
+        //Delete One Element Function
         newCardClose.addEventListener('click', () => {
             newCard.remove();
         })
 
-        newCardComplete.addEventListener('click', (event) => {
+        //Complete one Element Function
+        newCardComplete.addEventListener('click', () => {
             newCard.classList.toggle('complete-card-bg');
         })
     })
