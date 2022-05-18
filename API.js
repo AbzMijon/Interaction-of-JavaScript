7f2d23f77ab56769404a007f8cdc1528a205203c
AbzMijon <egorchiktop228@gmail.com>
2022-05-18 17:54:24 +0300

    finished game

--- API.js
+++
@@ -1,8 +0,0 @@
-const root = document.getElementById('root');
-const tx = document.getElementById('txarea');
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-    localStorage.setItem('txValue', tx.value);
-})
-tx.value = localStorage.getItem('txValue');
---
+++ script.js
@@ -0,0 +1,32 @@
+const body = document.getElementById('game-body');
+const box = document.getElementById('game-body__box');
+const btn = document.getElementById('game-body__start');
+const rulesMenu = document.querySelector('.game-body__rules');
+const burgerMenu = document.getElementById('burger');
+const burgerLines = document.getElementById('burger__line');
+
+body.addEventListener('click', event => {
+    if(event.target === burgerMenu || event.target === burgerLines) {
+        rulesMenu.classList.toggle('game-body__rules-click');
+    }
+    else if(event.target === btn) {
+        btn.style.display = 'none';
+        const timeBefore = new Date().getSeconds();
+        const coor = () => {
+            let math = Math.random() * 800;
+            box.style.display = 'block';
+            box.style.top = math + 'px';
+            box.style.left = math + 'px';
+        }
+        setTimeout(coor, 3000);
+        box.addEventListener('click', () => {
+            let resultTime = Math.abs(timeBefore - new Date().getSeconds()) - 3;
+            const result = document.createElement('div');
+            const resultNode = document.createTextNode(`Ваш результат ${resultTime} секунд/ы !`);
+            result.append(resultNode);
+            body.append(result);
+            result.className = 'result';
+            box.style.display = 'none';
+        })
+    }
+})
