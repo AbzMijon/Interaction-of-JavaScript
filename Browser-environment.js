6b90e226641098471df5c8203efc4d2a9da9b0bb
AbzMijon <egorchiktop228@gmail.com>
2022-05-19 17:33:21 +0300

    in progress

--- Browser-environment.js
+++ Browser-environment.js
@@ -1,22 +1,22 @@
 //Hi, everybody! Today Challendge Only JavaScript with CSS ------------------------------------------------------------------------------------
 
 
-//In The Beginning We Announce Variables..
-const mainRoot = document.getElementById('root');
-const toolsRow = document.createElement('div');
-const addBtn = document.createElement('button');
-const textAddBtn = document.createTextNode('Add');
-const theNote = document.createElement('input');
-const delBtn = document.createElement('button');
-const textDelBtn = document.createTextNode('Delete All');
+    //In The Beginning We Announce Variables..
+    const mainRoot = document.getElementById('root');
+    const toolsRow = document.createElement('div');
+    const addBtn = document.createElement('button');
+    const textAddBtn = document.createTextNode('Add');
+    const theNote = document.createElement('input');
+    const delBtn = document.createElement('button');
+    const textDelBtn = document.createTextNode('Delete All');
 
-//Then We Add To MainRoot Variables..
-mainRoot.append(toolsRow);
-addBtn.append(textAddBtn);
-toolsRow.append(addBtn);
-toolsRow.append(theNote);
-delBtn.append(textDelBtn);
-toolsRow.append(delBtn);
+    //Then We Add To MainRoot Variables..
+    mainRoot.append(toolsRow);
+    addBtn.append(textAddBtn);
+    toolsRow.append(addBtn);
+    toolsRow.append(theNote);
+    delBtn.append(textDelBtn);
+    toolsRow.append(delBtn);
 
 theNote.placeholder = 'Enter To Do...'; //For placeholder
 
@@ -27,20 +27,28 @@ theNote.placeholder = 'Enter To Do...'; //For placeholder
     theNote.className = 'root__note';
     delBtn.className = 'root__del-btn';
     
+    //API Locale Storage
+    let toDoArr = [];
+    let ids = 0;
+    const ToDoConstructor = function(toDoText, toDoId, toDoComplete) {
+        this.toDoText = toDoText;
+        this.toDoId = toDoId;
+        this.toDoComplete = toDoComplete;
+    }
 
-    //Add new card
-    addBtn.addEventListener('click', () => {
+    const addNewCard = inputValue => {
+        inputValue = theNote.value;  
         //Announce variables..
         const newCard = document.createElement('div');
         const newCardComplete = document.createElement('button');
         const newCardCompleteTxt = document.createTextNode('✔');
         const newCardToDo = document.createElement('div');
-        const newCardToDoTxt = document.createTextNode(theNote.value);
+        const newCardToDoTxt = document.createTextNode(inputValue);
         const newCardClose = document.createElement('button');
         const newCardCloseTxt = document.createTextNode('✖');
         const newCardDate = document.createElement('div');
         const newCardDateTxt = document.createTextNode(new Date().toDateString());
-        
+
         //Adding variables..
         newCardDate.append(newCardDateTxt);
         newCard.append(newCardDate);
@@ -51,6 +59,7 @@ theNote.placeholder = 'Enter To Do...'; //For placeholder
         newCardComplete.append(newCardCompleteTxt);
         newCard.append(newCardComplete);
         mainRoot.append(newCard)
+
         //Give name our variables for future working in CSS
         mainRoot.className = 'main__root';
         toolsRow.className = 'root__row';
@@ -63,32 +72,37 @@ theNote.placeholder = 'Enter To Do...'; //For placeholder
         newCardClose.className = 'root__close-btn';
         newCardDate.className = 'root__date-btn';
 
-        //API Storage
-        const textAPI = localStorage.setItem('text', theNote.value);
-
-        //Zero symbols in input
-        theNote.value = '';
-
+        const toDoApi = new ToDoConstructor(inputValue, ids++, false);
+        toDoArr.push(toDoApi);
+        localStorage.setItem('todoArr', JSON.stringify(toDoArr));
         //Events
 
-        //Delete All Function
         const arrCard = document.getElementsByClassName('root__new-card');
-        delBtn.addEventListener('click', () => {
-            for (const card of arrCard) {
-                card.remove();
+        mainRoot.addEventListener('click', (event) => {
+            if (event.target === delBtn) {
+                for (const card of arrCard) {
+                    card.remove();
+                }
             }
+            else if (event.target === newCardClose) {
+                newCard.remove();
+                localStorage.removeItem('todoArr');
+            }
+            else if (event.target === newCardComplete) {
+                newCard.classList.toggle('complete-card-bg');
+            }
+            else return;
         })
 
-        //Delete One Element Function
-        newCardClose.addEventListener('click', () => {
-            newCard.remove();
+        /* theNote.value = ''; */
+    }
+
+    addBtn.addEventListener('click', addNewCard);
+
+    //Add API element to HTML
+    const todoArrFromStorage = JSON.parse(localStorage.getItem('todoArr'));
+    if(todoArrFromStorage && todoArrFromStorage.length) {
+        todoArrFromStorage.forEach(element => {
+            mainRoot.append(addNewCard(element.toDoText));
         })
-
-        //Complete one Element Function
-        newCardComplete.addEventListener('click', () => {
-            newCard.classList.toggle('complete-card-bg');
-        })
-
-    })
-
-    console.log(localStorage.getItem('text'));
+    }
