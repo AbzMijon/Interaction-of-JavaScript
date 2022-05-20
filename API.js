8a95809dcb30497c1a763926d9de473d211944d0
AbzMijon <egorchiktop228@gmail.com>
2022-05-20 14:36:15 +0300

    practice module lesson

--- API.js
+++ API.js
@@ -1,3 +1,5 @@
+import { decktr } from './moduleCheck.js';
+
 const root = document.getElementById('root');
 const tx = document.getElementById('txarea');
 const btn = document.getElementById('btn');
@@ -6,3 +8,5 @@ btn.addEventListener('click', () => {
     localStorage.setItem('txValue', tx.value);
 })
 tx.value = localStorage.getItem('txValue');
+
+
