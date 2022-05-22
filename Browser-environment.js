1f87fb7768ae680be5b893983ccd1f2da0d49489
AbzMijon <egorchiktop228@gmail.com>
2022-05-22 16:07:26 +0300

    one back step

--- Browser-environment.js
+++ Browser-environment.js
@@ -1,11 +1,38 @@
 //Hi, everybody! Today Challendge Only JavaScript with CSS ------------------------------------------------------------------------------------
 
-//Imports
-import { mainRoot, addBtn, theNote, delBtn } from './MainVariable.js';
-import { ToDoConstructor } from './Constructor.js';
+//In The Beginning We Announce Variables..
+export const mainRoot = document.getElementById('root');
+export const toolsRow = document.createElement('div');
+export const addBtn = document.createElement('button');
+export const textAddBtn = document.createTextNode('Add');
+export const theNote = document.createElement('input');
+export const delBtn = document.createElement('button');
+export const textDelBtn = document.createTextNode('Delete All');
+
+//Then We Add To MainRoot Variables..
+mainRoot.append(toolsRow);
+addBtn.append(textAddBtn);
+toolsRow.append(addBtn);
+toolsRow.append(theNote);
+delBtn.append(textDelBtn);
+toolsRow.append(delBtn);
+
+theNote.placeholder = 'Enter To Do...'; //For placeholder
+
+//Give name our variables for future working in CSS
+mainRoot.className = 'main__root';
+toolsRow.className = 'root__row';
+addBtn.className = 'root__add-btn';
+theNote.className = 'root__note';
+delBtn.className = 'root__del-btn';
 
 //Array our future cards
 let toDoArr = []; //In this array in future we will add our cards
+export const ToDoConstructor = function (toDoText, toDoId, toDoComplete) { //It is a sample card
+	this.toDoText = toDoText;
+	this.toDoId = toDoId;
+	this.toDoComplete = toDoComplete;
+};
 
 //Creating layout of card
 const addNewCard = function (text, isChecked = false, id) {
--- Constructor.js
+++
@@ -1,5 +0,0 @@
-export const ToDoConstructor = function (toDoText, toDoId, toDoComplete) { //It is a sample card
-	this.toDoText = toDoText;
-	this.toDoId = toDoId;
-	this.toDoComplete = toDoComplete;
-};
