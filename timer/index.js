eedf4d0a69f859448538960294b0b3e46ca033d8
AbzMijon <egorchiktop228@gmail.com>
2022-05-27 19:34:06 +0300

    home task ??

--- timer/index.js
+++ timer/index.js
@@ -3,11 +3,15 @@ const startBtn = document.querySelector('.wrapper__btn--start');
 const stopBtn = document.querySelector('.wrapper__btn--stop');
 const result = document.querySelector('.wrapper__result');
 
-startBtn.addEventListener('click', (event) => {
+startBtn.addEventListener('click', () => {
     let i = 0;
     const startTimer = setInterval(() => {
-        const resultTxtNode = document.createTextNode(i++);
-        result.append(resultTxtNode);
+        i++
+        console.log(i);
+        let resultTxtNode = document.createTextNode(i);
+        result.append(resultTxtNode); //??
     }, 1000);
-    console.log(i);
+    stopBtn.addEventListener('click', () => {
+        const stopTimer = clearInterval(startTimer); 
+    })
 })
