b6c8555eca537913d00642499c9ca6518b944638
AbzMijon <egorchiktop228@gmail.com>
2022-05-28 15:16:45 +0300

    finished timer

--- timer/index.js
+++ timer/index.js
@@ -1,17 +1,86 @@
-//Anounce variables
-const startBtn = document.querySelector('.wrapper__btn--start');
-const stopBtn = document.querySelector('.wrapper__btn--stop');
-const result = document.querySelector('.wrapper__result');
+//Get times
+const miliSecondElem = document.querySelector('.miliseconds');
+const secondElem = document.querySelector('.seconds');
+const minuteElem = document.querySelector('.minutes');
+const hourElem = document.querySelector('.hours');
 
+//Get buttons
+const startBtn = document.querySelector('.start--btn');
+const pauseBtn = document.querySelector('.pause--btn');
+const stopBtn = document.querySelector('.stop--btn');
+
+//Add variable
+let hours = 00,
+	minutes = 00,
+	seconds = 00,
+	miliseconds = 00,
+	timer;
+
+//Events
 startBtn.addEventListener('click', () => {
-    let i = 0;
-    const startTimer = setInterval(() => {
-        i++
-        console.log(i);
-        let resultTxtNode = document.createTextNode(i);
-        result.append(resultTxtNode); //??
-    }, 1000);
-    stopBtn.addEventListener('click', () => {
-        const stopTimer = clearInterval(startTimer); 
-    })
-})
+	timer = setInterval(() => {
+		miliseconds++;
+		if (miliseconds < 9) {
+			miliseconds++;
+			miliSecondElem.innerText = '0' + miliseconds;
+		}
+		if (miliseconds > 9) {
+			miliseconds++;
+			miliSecondElem.innerText = miliseconds;
+		}
+		if (miliseconds > 99) {
+			miliseconds = 0;
+			miliSecondElem.innerText = '00';
+			seconds++;
+			if (seconds <= 9) {
+				secondElem.innerText = '0' + seconds;
+			}
+			if (seconds > 9) {
+				secondElem.innerText = seconds;
+			}
+			if (seconds > 59) {
+				seconds = 0;
+				secondElem.innerText = '00';
+				minutes++;
+				if (minutes <= 9) {
+					minuteElem.innerText = '0' + minutes;
+				}
+				if (minutes > 9) {
+					minuteElem.innerText = minutes;
+				}
+				if (minutes > 59) {
+					minuteElem.innerText = '00';
+					hours++;
+					if (hours <= 9) {
+						hourElem.innerText = '0' + hours;
+					}
+					if (hours > 9) {
+						hourElem.innerText = hours;
+					}
+					if (hours > 23) {
+						hourElem.innerText = '00';
+					}
+				}
+			}
+		}
+	}, 20);
+});
+
+pauseBtn.addEventListener('click', () => {
+	clearInterval(timer);
+});
+stopBtn.addEventListener('click', () => {
+	miliseconds = 0;
+	seconds = 0;
+	minutes = 0;
+	hours = 0;
+	miliSecondElem.innerText = '00';
+	secondElem.innerText = '00';
+	minuteElem.innerText = '00';
+	hourElem.innerText = '00';
+	clearInterval(timer);
+});
+
+const test = setInterval(() => {
+    console.log('1 minute is passed..');
+}, 60000);
