4f939e2d00610cbe7f89d0cb24bf7006597ee5b7
AbzMijon <egorchiktop228@gmail.com>
2022-05-29 17:09:36 +0300

    finished numbers

--- moduls.js
+++ moduls.js
@@ -29,3 +29,41 @@ const arrObj = [
         age:21,
     },
 ]
+
+//tets
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const num = document.querySelector('.how');
+
+
+let resultNum = 0;
+
+const add = () => {
+    resultNum++;
+    num.innerText = resultNum;
+    localStorage.setItem('num', resultNum);
+}
+plus.addEventListener('click', add);
+
+const disAdd = () => {
+    resultNum--;
+    num.innerText = resultNum;
+    if(resultNum < 0) {
+        num.innerText = 0;
+        resultNum = 0;
+    }
+    localStorage.setItem('num', resultNum);
+}
+minus.addEventListener('click', disAdd);
+
+const getStorage = () => {
+    if(localStorage.length > 0) {
+        const getStorageNum = localStorage.getItem('num');
+        num.innerText = getStorageNum;
+        resultNum = getStorageNum;
+    } else {
+        num.innerText = 0;
+        resultNum = 0;
+    }
+}
+getStorage();
