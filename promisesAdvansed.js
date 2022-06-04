719d2d45bd5a6cf086786cf8bb3cfe4a1a52c9a7
AbzMijon <egorchiktop228@gmail.com>
2022-06-04 18:42:34 +0300

    fix

--- promisesAdvansed.js
+++ promisesAdvansed.js
@@ -1,15 +1,21 @@
 const root = document.getElementById('root');
+const list = document.querySelector('.list');
 
+//Async / await
 const getPosts = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await response.json();
-    const titles1 = await posts[15].title;
-    const titles2 = await posts[23].title;
-    const titles3 = await posts[700].title;
-    const titles4 = await posts[3].title;
-    let arr = [titles1, titles2, titles3, titles4];
-    arr.forEach(elem => {
-        console.log(elem);
-    })
-}
-    getPosts().catch((err) => console.log(new Error(`Error is detected in: ${err}`)));
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const posts = await response.json();
+	const titles1 = await posts[15].title;
+	const titles2 = await posts[23].title;
+	const titles3 = await posts[7].title;
+	const titles4 = await posts[3].title;
+	let arr = [titles1, titles2, titles3, titles4];
+	arr.forEach((elem) => {
+		const item = document.createElement('li');
+		item.innerText = elem;
+		list.append(item);
+	});
+};
+getPosts().catch((err) =>
+	console.log(new Error(`Error is detected in: ${err}`))
+);
